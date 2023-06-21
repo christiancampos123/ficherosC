83d7e94efee84db3b32e54997b9dbfea1a0ddb9d
Christian Campos <christian.campos@kaicen.com>
2023-06-21 08:57:35 +0200

    fichero modificado y segundo fichero datos2

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
 void leerData();
+void meterData();
 int main() {
     leerData();
+    meterData();
     return 0;
 }
 
@@ -40,4 +43,48 @@ void leerData() {
     }
     // Cerrar el archivo
     fclose(archivo);
+}
+
+void meterData() {
+    FILE *archivoOriginal;
+    FILE *archivoNuevo;
+    char nombreArchivoOriginal[] = "C:/Users/christian.campos/Desktop/scriptsDeC/ficherosC/datos.txt";
+    char nombreArchivoNuevo[] = "C:/Users/christian.campos/Desktop/scriptsDeC/ficherosC/datos2.txt";
+    char linea[100];
+    int numLin = 0;
+    char numLins[50];
+
+    // Abrir archivo original en modo de lectura
+    archivoOriginal = fopen(nombreArchivoOriginal, "r");
+
+    // Verificar si el archivo original se ha abierto correctamente
+    if (archivoOriginal == NULL) {
+        printf("Error al abrir el archivo original.\n");
+    }
+
+    // Abrir archivo nuevo en modo de escritura
+    archivoNuevo = fopen(nombreArchivoNuevo, "w");
+
+    // Verificar si el archivo nuevo se ha abierto correctamente
+    if (archivoNuevo == NULL) {
+        printf("Error al abrir el archivo nuevo.\n");
+
+    }
+    // Leer línea por línea del archivo original, agregar la cadena nueva y escribir en el archivo nuevo
+    while (fgets(linea, sizeof(linea), archivoOriginal) != NULL) {
+        char lineaModificada[150]; // Espacio para almacenar la línea modificada
+        numLin++;
+
+        sprintf(numLins, "%d", numLin);
+        strcpy(lineaModificada, numLins); // Copiar la cadena nueva al principio de la línea
+        strcat(lineaModificada, linea); // Concatenar la línea original después de la cadena nueva
+
+        fputs(lineaModificada, archivoNuevo); // Escribir la línea modificada en el archivo nuevo
+    }
+
+    // Cerrar archivos
+    fclose(archivoOriginal);
+    fclose(archivoNuevo);
+
+    printf("Archivo creado con las modificaciones.\n");
 }
